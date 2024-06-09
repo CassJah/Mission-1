66945b0aaf16e8fc480ed9b753e1bd8801eadd98
CassJah <144091819+CassJah@users.noreply.github.com>
2024-06-09 16:26:53 +1200

    header responsiveness

--- mission-1/src/components/Header.jsx
+++ mission-1/src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import "./Header.css";
+import React, { useState } from "react";
 import logo from "../../src/assets/turners 22-updated.jpg";
+import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="top-nav">
@@ -10,15 +16,26 @@ function Header() {
           <a href="#" className="logo">
             <img src={logo} alt="Turners Cars" />
           </a>
-          <div className="top-links">
-            <a href="#">LOGIN OR REGISTER</a>
-            <a href="#">0800 887 637</a>
-            <a href="#">Find Us</a>
-            <a href="#">中文</a>
+          <div className="right-section">
+            <div className="top-links">
+              <a href="#">
+                <i className="fas fa-map-marker-alt"></i> <span>Find Us</span>
+              </a>
+              <a href="#">
+                <i className="fas fa-sign-in-alt"></i>{" "}
+                <span>LOGIN OR REGISTER</span>
+              </a>
+              <a href="#">
+                <i className="fas fa-phone"></i> <span>0800 887 637</span>
+              </a>
+            </div>
+            <div className="hamburger" onClick={toggleMenu}>
+              <i className="fas fa-bars"></i>
+            </div>
           </div>
         </div>
       </div>
-      <div className="main-nav">
+      <div className={`main-nav ${menuOpen ? "open" : ""}`}>
         <div className="container">
           <nav>
             <a href="#">Find a Car</a>
@@ -31,6 +48,23 @@ function Header() {
           </nav>
         </div>
       </div>
+      <div className="mobile-search-bar">
+        <input type="text" placeholder="Search..." />
+        <button className="search-button">Search</button>
+      </div>
+      {menuOpen && (
+        <div className="mobile-menu">
+          <nav>
+            <a href="#">Find a Car</a>
+            <a href="#">How to Buy</a>
+            <a href="#">Sell your Car</a>
+            <a href="#">Finance & Insurance</a>
+            <a href="#">Turners Subscription</a>
+            <a href="#">Auctions</a>
+            <a href="#">Services & Info</a>
+          </nav>
+        </div>
+      )}
     </header>
   );
 }
